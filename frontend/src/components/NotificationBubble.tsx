5c295595a5d31b46dfad0393e43a11099a062b25
JZ-Zhou-UofC <john.zhou1@ucalgary.ca>
2026-03-18 09:49:33 -0600

    notification bubble

---
+++ frontend/src/components/NotificationBubble.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import { Snackbar, Alert, AlertColor } from "@mui/material";
+
+interface NotificationBubbleProps {
+  open: boolean;
+  message: string;
+  severity: AlertColor; // "error" | "warning" | "info" | "success"
+  onClose: (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => void;
+}
+
+const NotificationBubble: React.FC<NotificationBubbleProps> = ({
+  open,
+  message,
+  severity,
+  onClose,
+}) => {
+  return (
+    <Snackbar
+      open={open}
+      autoHideDuration={5000}
+      onClose={onClose}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+    >
+      <Alert
+        onClose={onClose}
+        severity={severity}
+        sx={{
+          padding: "20px",
+          fontSize: "1.5rem",
+        }}
+      >
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default NotificationBubble;
