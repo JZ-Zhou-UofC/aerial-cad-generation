9849a5c1a8df56de0babc6d55df7874ff12cfdb2
JZ-Zhou-UofC <john.zhou1@ucalgary.ca>
2026-03-30 15:32:53 -0600

    next cache issue

--- .next/dev/static/chunks/src_app_page_tsx_5676ca04._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_939a1c68._.js",
-  "static/chunks/src_dbfa6662._.js"
-],
-    source: "dynamic"
-});
