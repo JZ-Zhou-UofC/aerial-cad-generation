391b6f1516e4acb2478b86523b38c6246dfa0dbc
JZ-Zhou-UofC <john.zhou1@ucalgary.ca>
2026-03-30 15:21:46 -0600

    frontend not rendering

---
+++ .next/dev/static/chunks/src_app_page_tsx_5676ca04._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_939a1c68._.js",
+  "static/chunks/src_dbfa6662._.js"
+],
+    source: "dynamic"
+});
